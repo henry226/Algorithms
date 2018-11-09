dac3872a0cf3bff63c59129aefa2f2298adc0c0f
henry226 <henry5581984@gmail.com>
2018-11-09 11:39:43 -0500

    added mergeSort function. It divides array into leftArray and rightArray

--- MergeSort.java
+++ MergeSort.java
@@ -4,19 +4,55 @@ public class MergeSort {
 	public static void main(String[] args) {
 		int array[] = {7, 9, 1, 5, 6, 4}; // initial array value
 		
-		System.out.println("Before merge sort:");
+		System.out.print("Before merge sort: ");
 		for(int a : array) {
 			System.out.print(a + " ");
 		}
 		
 		System.out.println("\nSorting...");
-		//array = insertSort(array);
+		array = mergeSort(array);
 		
-		System.out.println("After merge sort:");
+		System.out.print("After merge sort: ");
 		for(int a : array) {
 			System.out.print(a + " ");
 		}
 
+	}
+	
+	public static int[] mergeSort(int[] array) {
+		int n = array.length; // total array length
+		
+		// return array if there is only one or zero elements
+		if(n <= 1) return array;
+		
+		int middleNumber = n/2; // the middle number of the array
+		
+		int[] leftArray = new int[middleNumber]; // declare of left array
+		int[] rightArray;
+		// check if odd number of array, plus one array length
+		if(n%2 != 0) {
+			rightArray = new int[middleNumber+1];
+		}
+		else {
+			rightArray = new int[middleNumber];
+		}
+		
+		System.out.print("The left array: ");
+		// stored the leftArray from array
+		for(int i=0; i<middleNumber; i++) {
+			leftArray[i] = array[i];
+			System.out.print(leftArray[i] + " ");
+		}
+		
+		System.out.print("\nThe right array: ");
+		// stored the rightArray from array
+		for(int i=middleNumber; i<n; i++) {
+			rightArray[i-middleNumber] = array[i];
+			System.out.print(rightArray[i-middleNumber] + " ");
+		}
+		
+		System.out.println();
+		return array;
 	}
 
 }
